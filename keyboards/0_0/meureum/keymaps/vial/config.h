51c8b8f2e43e0661e1d360bed62d8f82ed13ad03
Alabahuy <rezolutionrez@gmail.com>
2024-07-21 13:25:52 +0700

    add

---
+++ keyboards/0_0/meureum/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+// Copyright 2023 Alabahuy (@Alabahuy)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xF2, 0xCC, 0x35, 0x13, 0xF0, 0xD0, 0xCD, 0x58}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 1, 0 }
